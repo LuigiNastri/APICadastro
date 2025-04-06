c377f2cd72daf8c12ffbd74fbe3cb814cd92370c
Luigi Nastri <luiginastri.dev@gmail.com>
2025-04-06 13:17:33 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no FuncionarioMapper os caminhos entidades x DTO

--- src/main/java/dev/nastriluigi/CadastroDeFuncionarios/Funcionarios/FuncionarioMapper.java
+++ src/main/java/dev/nastriluigi/CadastroDeFuncionarios/Funcionarios/FuncionarioMapper.java
@@ -1,4 +1,31 @@
 package dev.nastriluigi.CadastroDeFuncionarios.Funcionarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FuncionarioMapper {
+
+    public FuncionarioModel map(FuncionarioDTO funcionarioDTO){
+        FuncionarioModel funcionarioModel = new FuncionarioModel();
+        funcionarioModel.setID(funcionarioDTO.getID());
+        funcionarioModel.setNome(funcionarioDTO.getNome());
+        funcionarioModel.setEmail(funcionarioDTO.getEmail());
+        funcionarioModel.setIdade(funcionarioDTO.getIdade());
+        funcionarioModel.setCargo(funcionarioDTO.getCargo());
+        funcionarioModel.setAtividade(funcionarioDTO.getAtividade());
+
+        return funcionarioModel;
+    }
+
+    public FuncionarioDTO map(FuncionarioModel funcionarioModel){
+        FuncionarioDTO funcionarioDTO = new FuncionarioDTO();
+        funcionarioDTO.setID(funcionarioModel.getID());
+        funcionarioDTO.setNome(funcionarioModel.getNome());
+        funcionarioDTO.setEmail(funcionarioModel.getEmail());
+        funcionarioDTO.setIdade(funcionarioModel.getIdade());
+        funcionarioDTO.setCargo(funcionarioModel.getCargo());
+        funcionarioDTO.setAtividade(funcionarioModel.getAtividade());
+        return funcionarioDTO;
+    }
+
 }
